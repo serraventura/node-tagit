fdae808a791dfb36bf189753ddd8899fad0f45f3
Thiago Ventura <serraventura@gmail.com>
2019-08-26 21:47:25 +0100

    BUGFIX-838383 - more test coverage

--- index.test.js
+++ index.test.js
@@ -6,4 +6,5 @@ describe("", () => {
   it("should bump PATCH twice but not MINOR", () => {});
   it("should NOT bump PATCH or MINOR when MAJOR is bumped", () => {});
   it("should keep bumping release tag (release_*) regardless features or bug fixes", () => {});
+  it("should recognise bugfix, feature with extra spaces on naming convention", () => {});
 });
