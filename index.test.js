9ca4e8f244ef8307562fbd9ed4216809ffd4895f
Thiago Ventura <serraventura@gmail.com>
2019-08-26 21:38:39 +0100

    F-42342 - initial tests

---
+++ index.test.js
@@ -0,0 +1,8 @@
+describe("", () => {
+  it("should create the first version if no tag found", () => {});
+  it("should bump MINOR but not PATCH", () => {});
+  it("should bump MINOR twice but not PATCH", () => {});
+  it("should bump PATCH but not MINOR", () => {});
+  it("should bump PATCH twice but not MINOR", () => {});
+  it("should NOT bump PATCH or MINOR when MAJOR is bumped", () => {});
+});
