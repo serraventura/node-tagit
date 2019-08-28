03cdb1a17aea5e576aaae2d501e958642cee5345
Thiago Ventura <v-thiago.ventura@zoopla.co.uk>
2019-08-28 13:09:54 +0100

    more test coverage

--- index.test.js
+++ index.test.js
@@ -7,4 +7,5 @@ describe("", () => {
   it("should NOT bump PATCH or MINOR when MAJOR is bumped", () => {});
   it("should keep bumping release tag (release_*) regardless features or bug fixes", () => {});
   it("should recognise bugfix, feature with extra spaces on naming convention", () => {});
+  it("should hold exceptional cases as 1.0.0-alpha, 1.0.0-alpha.1, 1.0.0-0.3.7, 1.0.0-x.7.z.92, 1.0.0+20130313144700", () => {});
 });
